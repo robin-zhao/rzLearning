b11452780cce65dc2df9c8f41b7a07852caba826
Jian Zhao <jzhao@astreetpress.com>
2014-12-10 13:54:48 +0800

    try import

---
+++ import/importExample.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#
+# @author Jian Zhao <boborabit@gmail.com>
+#
+# @description 
+#
+
+from robin.example import function1
+#from robin.example import *
+function1()
+
+import robin.example2
+robin.example2.function2()
---
+++ import/robin/example2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#
+# @author Jian Zhao <boborabit@gmail.com>
+#
+# @description 
+#
+
+def function2():
+    print("imported from example2");
