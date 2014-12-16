51519e941d09c6226b3258f80138a1dc72267e99
Jian Zhao <jzhao@astreetpress.com>
2014-12-16 17:10:52 +0800

    check window object

---
+++ Nodejs/javascript/browser/window.js
@@ -0,0 +1,11 @@
+console.log(window);
+
+console.log(window.document);
+
+console.log(window.history);
+
+console.log(window.location);
+
+console.log(window.navigator);
+
+console.log(window.screen);
