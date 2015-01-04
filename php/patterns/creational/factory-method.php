50d8f52716c27eeb76289725396970bd2abc5772
Jian Zhao <jzhao@astreetpress.com>
2015-01-04 22:36:00 +0800

    simple factory + factory method

---
+++ php/patterns/creational/factory-method.php
@@ -0,0 +1,51 @@
+<?php
+abstract class Creator
+{
+    abstract public function factory();
+}
+
+class HumanCreator extends Creator
+{
+    public function factory()
+    {
+        return new Human();
+    }
+}
+
+abstract class Creature
+{
+    abstract public function getName();
+}
+
+class Human extends Creature
+{
+    public function getName()
+    {
+        return 'Human';
+    }   
+}
+
+$creator = new HumanCreator();
+$creature = $creator->factory();
+var_dump($creature->getName());
+
+class DogCreator extends Creator
+{
+    public function factory()
+    {
+        return new Dog();
+    }
+}
+
+class Dog extends Creature
+{
+    public function getName()
+    {
+        return 'Dog';
+    }
+}
+
+$creator2 = new DogCreator();
+$creature = $creator2->factory();
+var_dump($creature->getName());
+
